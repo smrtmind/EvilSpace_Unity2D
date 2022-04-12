ff4780bdf741b57434685f1de3740c8b4b5bda8b
Oleg Gvozdetskyi <oleg.gvozdetskyi@gmail.com>
2022-04-12 15:19:56 +0300

    fix movement, add effects to player

---
+++ Assets/Scripts/DestroyObjectComponent.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Scripts
+{
+    public class DestroyObjectComponent : MonoBehaviour
+    {
+        [SerializeField] private GameObject _objectToDestroy;
+
+        public void DestroyObject()
+        {
+            Destroy(_objectToDestroy);
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,15 @@ namespace Scripts
     {
         [SerializeField] private float _rotationSpeed = 150;
         [SerializeField] private float _burstSpeed = 1;
+        [SerializeField] private GameObject _leftStarterFlame;
+        [SerializeField] private GameObject _rightStarterFlame;
 
         private Animator _animator;
 
-        private readonly int LeftTurnKey = Animator.StringToHash("left-turn");
-        private readonly int RightTurnKey = Animator.StringToHash("right-turn");
+        private readonly int LeftStarterKey = Animator.StringToHash("left-turn");
+        private readonly int RightStarterKey = Animator.StringToHash("right-turn");
 
-        //public float rotation { get; set; }
+        public float rotation { get; set; }
         public float burst { get; set; }
         public bool shoot { get; set; }
 
@@ -26,38 +28,61 @@ namespace Scripts
 
         private void FixedUpdate()
         {
-            if (Input.GetKey(KeyCode.RightArrow))
-            {
-                _rigidbody.angularVelocity = -1 * _rotationSpeed;
-                _animator.SetBool(RightTurnKey, true);
-            }
-            else if (Input.GetKey(KeyCode.LeftArrow))
+            var isLeftPressed = Input.GetKey(KeyCode.LeftArrow);
+            var isRightPressed = Input.GetKey(KeyCode.RightArrow);
+            var isMovingForward = burst > 0;
+
+            if (isLeftPressed)
             {
                 _rigidbody.angularVelocity = 1 * _rotationSpeed;
-                _animator.SetBool(LeftTurnKey, true);
+
+                PlayAnimation(LeftStarterKey);
+                ActivateObject(_rightStarterFlame);
             }
+            else if (isRightPressed)
+            {
+                _rigidbody.angularVelocity = -1 * _rotationSpeed;
 
-            //if (rotation > 0)
-            //{
-            //    _rigidbody.angularVelocity = rotation * _rotationSpeed;
-            //    _animator.SetBool(RightTurnKey, true);
-            //}
-            //else if (rotation < 0)
-            //{
-            //    _rigidbody.angularVelocity = -rotation * _rotationSpeed;
-            //    _animator.SetBool(LeftTurnKey, true);
-            //}
-
+                PlayAnimation(RightStarterKey);
+                ActivateObject(_leftStarterFlame);
+            }
             else
             {
-                _animator.SetBool(LeftTurnKey, false);
-                _animator.SetBool(RightTurnKey, false);
+                PlayAnimation(disableAll: true);
+                ActivateObject(disableAll: true);
             }
 
-            if (burst > 0)
+            if (isMovingForward)
             {
                 Vector2 burstDelta = transform.up * _burstSpeed;
                 _rigidbody.velocity += burstDelta;
+
+                ActivateObject(disableAll: false);
+            }
+        }
+
+        private void PlayAnimation(int animation = default, bool disableAll = false)
+        {
+            _animator.SetBool(LeftStarterKey, false);
+            _animator.SetBool(RightStarterKey, false);
+
+            if (!disableAll)
+                _animator.SetBool(animation, true);
+        }
+
+        private void ActivateObject(GameObject go = null, bool disableAll = false)
+        {
+            SetGeneralState(false);
+
+            if (go != null)
+                go.SetActive(true);
+            else
+                SetGeneralState(disableAll ? false : true);
+
+            void SetGeneralState(bool state)
+            {
+                _leftStarterFlame.SetActive(state);
+                _rightStarterFlame.SetActive(state);
             }
         }
     }
