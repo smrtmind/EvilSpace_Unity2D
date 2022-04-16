5f5aed9b53952380c3d982bcb75965cf0d82429e
Oleg Gvozdetskyi <oleg.gvozdetskyi@gmail.com>
2022-04-16 20:29:25 +0300

    add hud elements

--- Assets/Scripts/GameSession.cs
+++ Assets/Scripts/GameSession.cs
@@ -4,21 +4,28 @@ namespace Scripts
 {
     public class GameSession : MonoBehaviour
     {
+        [SerializeField] private int _tries;
         [SerializeField] private int _health;
         [SerializeField] private int _score;
         [SerializeField] private HealthComponent _targetHp;
 
+        public int Tries => _tries;
         public int Health => _health;
         public int Score => _score;
+
+        public void AddScore(int score)
+        {
+            _score += score;
+        }
+
+        public void ModifyTries(int tries)
+        {
+            _tries += tries;
+        }
 
         private void Update()
         {
             _health = _targetHp.Health;
-        }
-
-        public void AddScore(int value)
-        {
-            _score += value;
         }
     }
 }
---
+++ Assets/Scripts/HudController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Scripts
+{
+    public class HudController : MonoBehaviour
+    {
+        [SerializeField] private Text _triesText;
+        [SerializeField] private Slider _healthBar;
+        [SerializeField] private Text _scoreText;
+
+        private GameSession _gameSession;
+
+        private void Start()
+        {
+            _gameSession = FindObjectOfType<GameSession>();
+        }
+
+        private void FixedUpdate()
+        {
+            _healthBar.value = _gameSession.Health;
+            _scoreText.text = _gameSession.Score.ToString();
+            _triesText.text = $"x {_gameSession.Tries}";
+        }
+    }
+}
