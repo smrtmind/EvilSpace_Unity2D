a2c50068f174136733f30b769fc054bdc54bbfaa
Oleg Gcozdetskyi <oleg.gvozdetskyi@gmail.com>
2023-03-21 17:07:29 +0200

    flame scale optimization

--- Assets/CodeBase/Animation/PlayerAnimationController.cs
+++ Assets/CodeBase/Animation/PlayerAnimationController.cs
@@ -10,7 +10,12 @@ namespace CodeBase.Animation
     {
         [SerializeField] private Animator animator;
         [field: SerializeField] public float TurnDetectIndent { get; private set; } = 0.5f;
-        [SerializeField] private float scaleFlameOnY = 0.15f;
+
+        [Header("Flame Settings")]
+        [SerializeField] private float scaleFlameDuration = 0.15f;
+        [SerializeField] private float minFlameScale;
+        [SerializeField] private float maxFlameScale;
+        [SerializeField] private float averageFlameScale;
         [SerializeField] private List<Transform> starterFlames;
         [SerializeField] private List<StarterFlameParameters> flameParameters;
 
@@ -18,6 +23,9 @@ namespace CodeBase.Animation
 
         private MovementState previousState;
         private bool flameIsScaling;
+        private float currentScaleValue;
+        private float previousScaleValue;
+        private Vector3 newFlameScale;
 
         public void UpdateAnimation(float directionX)
         {
@@ -33,18 +41,19 @@ namespace CodeBase.Animation
             else if (directionX > TurnDetectIndent && directionX != 0f)
                 ChangeFlamePosition(MovementState.Right);
             else
-                ChangeFlamePosition(MovementState.Moveless);           
+                ChangeFlamePosition(MovementState.Moveless);
         }
 
         private void ChangeFlamePosition(MovementState state)
         {
-            if (previousState == state) return;
+            if (state != previousState)
+            {
+                previousState = state;
+                var currentPoints = GetCurrentFlamePoints(state);
 
-            previousState = state;
-            var currentPoints = GetCurrentFlamePoints(state);
-
-            for (int i = 0; i < starterFlames.Count; i++)
-                starterFlames[i].position = currentPoints[i].position;
+                for (int i = 0; i < starterFlames.Count; i++)
+                    starterFlames[i].position = currentPoints[i].position;
+            }
         }
 
         private List<Transform> GetCurrentFlamePoints(MovementState state)
@@ -62,24 +71,22 @@ namespace CodeBase.Animation
 
         public void ScaleFlame(float directionY)
         {
-            if (!flameIsScaling)
+            if (directionY < TurnDetectIndent && directionY != 0f)
+                currentScaleValue = minFlameScale;
+            else if (directionY > TurnDetectIndent && directionY != 0f)
+                currentScaleValue = maxFlameScale;
+            else
+                currentScaleValue = averageFlameScale;
+
+            if (currentScaleValue != previousScaleValue && !flameIsScaling)
             {
+                previousScaleValue = currentScaleValue;
                 flameIsScaling = true;
 
-                if (directionY < TurnDetectIndent && directionY != 0f)
+                foreach (Transform flamePivot in starterFlames)
                 {
-                    foreach (Transform flamePivot in starterFlames)
-                        flamePivot.DOScale(new Vector3(flamePivot.localScale.x, 0.5f, flamePivot.localScale.z), scaleFlameOnY).OnComplete(() => flameIsScaling = false);
-                }
-                else if (directionY > TurnDetectIndent && directionY != 0f)
-                {
-                    foreach (Transform flamePivot in starterFlames)
-                        flamePivot.DOScale(new Vector3(flamePivot.localScale.x, 2f, flamePivot.localScale.z), scaleFlameOnY).OnComplete(() => flameIsScaling = false);
-                }
-                else
-                {
-                    foreach (Transform flamePivot in starterFlames)
-                        flamePivot.DOScale(new Vector3(flamePivot.localScale.x, 1f, flamePivot.localScale.z), scaleFlameOnY).OnComplete(() => flameIsScaling = false);
+                    newFlameScale = new Vector3(flamePivot.localScale.x, currentScaleValue, flamePivot.localScale.z);
+                    flamePivot.DOScale(newFlameScale, scaleFlameDuration).OnComplete(() => flameIsScaling = false);
                 }
             }
         }
